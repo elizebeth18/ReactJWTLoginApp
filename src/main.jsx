271ac9822261a714caca2ad787a399092efd08c3
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2025-05-20 08:27:48 +0530

    create login app using vite

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Routing from './components/Routing'
+
+createRoot(document.getElementById('root')).render(
+    <Routing />
+)
